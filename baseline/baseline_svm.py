e9f3ea210b5d59cb76545ddac587c9414cb04217
Peter Dun <bodun@stanford.edu>
2018-05-16 14:14:09 -0700

    finished data loading code

--- baseline/baseline_svm.py
+++ baseline/baseline_svm.py
@@ -1,3 +1,5 @@
+import load_data
+
 import os
 import numpy as np
 import scipy.ndimage
@@ -5,19 +7,20 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-DATA_PATH = "../sequence"
+def load_baseline_data():
+    data_map = load_data.get_data()
+    np_images = []
+    labels = []
+    for key in data_map:
+        data_list = data_map[key]
+        for index, path, label in data_list:
+            np_image = scipy.ndimage.imread(path).flatten()
+            np_images.append(np_image)
+            labels.append(label)
 
-def load_data():
-  image_files = os.listdir(DATA_PATH)
-  sorted_files = sorted(image_files)
-  np_images = []
-  for file_name in sorted_files:
-    file_path = os.path.join(DATA_PATH, file_name)
-    np_image = scipy.ndimage.imread(file_path).flatten()
-    np_images.append(np_image)
-  return np_images, np.asarray([4] * len(np_images))
+    return np_images, labels
 
-x, y = load_data()
+x, y = load_baseline_data()
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, stratify=y)
 clf = svm.SVC()
 clf.fit(x_train, y_train)
---
+++ baseline/load_data.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import collections
+
+DIR_PATH = '../data'
+
+STRESS = set(['str', 'nec', 'sur'])
+NOT_STRESS = set(['int', 'sce', 'bre'])
+
+def get_data():
+    data_map = collections.defaultdict(list)
+
+    for file_name in os.listdir(DIR_PATH):
+        info = file_name.split('.')[0].split('_')
+        num = int(info[0][5:])
+        subject = int(info[1])
+        scene = info[2]
+        task = info[3]
+        data_map[(subject, scene, task)].append((num, os.path.join(DIR_PATH, file_name), task in STRESS))
+
+    for key in data_map:
+        data_map[key].sort(key=lambda x:x[0])
+
+    return data_map
+
